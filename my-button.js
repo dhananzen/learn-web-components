9e3339c0b7d0dd1d017b90ba6f054fff98cdd4af
dhananzen <dhananjayk.456@gmail.com>
2020-01-16 11:06:14 +0530

    try templates

--- my-button.js
+++ my-button.js
@@ -1,34 +1,19 @@
 class MyButton extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({mode: 'open'})
+        this.attachShadow({ mode: 'open' })
     }
 
     connectedCallback() {
-        const varient = this.getAttribute('varient');
-        console.log(varient)
+        const btnTemplate =  document.querySelector("#my-button-template");
+        const children = btnTemplate.content.cloneNode(true);
 
-        const button = document.createElement("button");
-        button.innerText = "Click me!"
+        const button =  children.querySelector('button');
+
+        const varient = this.getAttribute('varient');
         button.classList.add(varient)
 
-        const styles = document.createElement('style');
-        styles.textContent = `
-            button {
-                border-radius: 10px;
-            }
-
-            .primary {
-                background-color: skyblue;
-            }
-        
-            .secondary {
-                background-color: wheat;
-            }
-        `
-
-        this.shadowRoot.appendChild(button);
-        this.shadowRoot.appendChild(styles);
+        this.shadowRoot.appendChild(children);
     }
 }
 
